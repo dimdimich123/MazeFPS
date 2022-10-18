9563b4b331e734cc0dfa37dd02d3cf4b6c84cf01
dimdimich123 <frostm333@gmail.com>
2022-10-18 14:16:43 +0700

    Created movement controller using keyboard and mouse

---
+++ Assets/Scripts/GameCore/Player/Control/KeyboardAndMouse.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace GameCore.Player.Control
+{
+    public sealed class KeyboardAndMouse : MovementController
+    {
+        private const string _moveHorizontal = "Horizontal";
+        private const string _moveVertical = "Vertical";
+        private float _moveX;
+        private float _moveZ;
+        private Vector3 _move;
+
+        private const string _mouseHorizontal = "Mouse X";
+        private const string _mouseVertical = "Mouse Y";
+        private float _mouseX;
+        private float _mouseY;
+        private float _xRotation = 0f;
+
+        protected override void MovePlayer()
+        {
+            _moveX = Input.GetAxis(_moveHorizontal);
+            _moveZ = Input.GetAxis(_moveVertical);
+
+            _move = _transform.right * _moveX + _transform.forward * _moveZ;
+
+            _characterController.Move(_move * _speed * Time.deltaTime);
+        }
+
+        protected override void RotateCamera()
+        {
+            _mouseX = Input.GetAxis(_mouseHorizontal) * _sensitivity * Time.deltaTime;
+            _mouseY = Input.GetAxis(_mouseVertical) * _sensitivity * Time.deltaTime;
+
+            _xRotation -= _mouseY;
+            _xRotation = Mathf.Clamp(_xRotation, -90, 90);
+
+            _transform.Rotate(Vector3.up * _mouseX);
+            _cameraTransform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        }
+    }
+}
+
