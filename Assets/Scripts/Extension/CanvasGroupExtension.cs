7dac403cd8315d68e1600a0d406caf7ebfb233d7
dimdimich123 <frostm333@gmail.com>
2022-10-28 13:36:58 +0700

    Created extension for CanvasGroup

---
+++ Assets/Scripts/Extension/CanvasGroupExtension.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public static class CanvasGroupExtension
+{
+    public static void Open(this CanvasGroup canvas)
+    {
+        canvas.alpha = 1;
+        canvas.interactable = true;
+        canvas.blocksRaycasts = true;
+    }
+
+    public static void Close(this CanvasGroup canvas)
+    {
+        canvas.alpha = 0;
+        canvas.interactable = false;
+        canvas.blocksRaycasts = false;
+    }
+}
