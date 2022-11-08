fbb71b4e03b110ae83fc5ae375f16dc1d0cf4cce
dimdimich123 <frostm333@gmail.com>
2022-11-08 18:32:46 +0700

    Changed characteristics of enemies and fixed scripts

--- Assets/Scripts/Configs/LevelConfig.cs
+++ Assets/Scripts/Configs/LevelConfig.cs
@@ -7,14 +7,14 @@ namespace Configs
     [CreateAssetMenu(fileName = "NewLevelConfig", menuName = "LevelConfig", order = 3)]
     public class LevelConfig : ScriptableObject
     {
-        [SerializeField] private SceneName _sceneName = SceneName.Level;
+        [SerializeField] private LevelNumber _levelNumber;
         [SerializeField] private MazeConfig _mazeConfig;
         [SerializeField] private PlayerConfig _playerConfig;
         [SerializeField] private int _enemiesAtSameTime;
         [SerializeField] private EnemyCreationData[] _enemiesData;
         [SerializeField] private float _timebetweenSpawnBonus;
 
-        public SceneName SceneName => _sceneName;
+        public LevelNumber LevelNumber => _levelNumber;
         public MazeConfig MazeConfig => _mazeConfig;
         public PlayerConfig PlayerConfig => _playerConfig;
         public int EnemiesAtSameTime => _enemiesAtSameTime;
--- Assets/Scripts/Infrastructure/Configs/ConfigsProvider.cs
+++ Assets/Scripts/Infrastructure/Configs/ConfigsProvider.cs
@@ -11,19 +11,19 @@ namespace Infrastructure.Configs
         private readonly LoadLevelOption _loadLevelOption;
         private readonly MovementControllerConfig _controllerConfig;
         private readonly Dictionary<EnemyTypeId, EnemyConfig> _enemiesConfigs = new Dictionary<EnemyTypeId, EnemyConfig>();
-        private readonly Dictionary<SceneName, LevelConfig> _levelsConfigs = new Dictionary<SceneName, LevelConfig>();
+        private readonly Dictionary<LevelNumber, LevelConfig> _levelsConfigs = new Dictionary<LevelNumber, LevelConfig>();
 
         public ConfigsProvider()
         {
             _loadLevelOption = Resources.Load<LoadLevelOption>("Configs/LoadLevelOption");
             _controllerConfig = Resources.Load<MovementControllerConfig>("Configs/MovementControllerConfig");
             _enemiesConfigs = Resources.LoadAll<EnemyConfig>("Configs/Enemies").ToDictionary(x => x.EnemyTypeId, x => x);
-            _levelsConfigs = Resources.LoadAll<LevelConfig>("Configs/Levels").ToDictionary(x => x.SceneName, x => x);
+            _levelsConfigs = Resources.LoadAll<LevelConfig>("Configs/Levels").ToDictionary(x => x.LevelNumber, x => x);
         }
 
         public LoadLevelOption LoadLevel =>_loadLevelOption;
         public MovementControllerConfig ControllerConfig => _controllerConfig;
         public EnemyConfig GetEnemyConfig(EnemyTypeId id) => _enemiesConfigs.TryGetValue(id, out EnemyConfig config) ? config : null;
-        public LevelConfig GetLevelConfig(SceneName name) => _levelsConfigs.TryGetValue(name, out LevelConfig config) ? config : null;
+        public LevelConfig GetLevelConfig(LevelNumber number) => _levelsConfigs.TryGetValue(number, out LevelConfig config) ? config : null;
     }
 }
